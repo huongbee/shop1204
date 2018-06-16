b6ced2324deed8d83a932742b7c64ec716c8a31f
Huong <admin@Apples-MacBook-Pro.local>
2018-06-16 19:54:48 +0700

    sql menu

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,7 +1,12 @@
 <?php
+include_once "model/BaseModel.php";
 class Controller{
 
     function loadView($nameView, $data=[]){
+        $model = new BaseModel;
+        $menu = $model->getCategories();
+        print_r($menu);
+        die;
         include_once "view/layout.view.php";
     }
 }
