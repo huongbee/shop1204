ef790db994a0af0af1a2653f986c13a2dc5f7706
Huong <admin@Apples-MacBook-Pro.local>
2018-06-19 21:18:11 +0700

    bestSeller

--- model/IndexModel.php
+++ model/IndexModel.php
@@ -17,6 +17,20 @@ class IndexModel extends DBConnect{
         return $this->loadMoreRows($sql);        
     }
 
+    function selectBestSeller(){
+        $sql = "SELECT p.*,u.url, SUM(d.quantity) as soluong
+                FROM bill_detail d
+                INNER JOIN products p 
+                ON d.id_product = p.id
+                INNER JOIN page_url u 
+                ON u.id = p.id_url
+                GROUP BY p.id
+                ORDER BY soluong DESC
+                LIMIT 0,10";
+        return $this->loadMoreRows($sql);        
+        
+    }
+
 }
 
 
